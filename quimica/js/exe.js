a04b0398b64d18611ea43253f3e182909384b01e
maysa195 <57270886+maysa195@users.noreply.github.com>
2021-03-12 23:47:53 -0300

    Add files via upload

---
+++ quimica/js/exe.js
@@ -0,0 +1,252 @@
+$(function(){
+    
+    $("#botao1").click(function(){
+        if($("#a").prop("checked") == true)
+        {
+            $("#resultado").text("Resposta incorreta:")
+            $("#r").append($("<p>").text("Alternativa D"));
+            $("#r").append($("<p>").text("Os hidrocarbonetos constituintes do gás GLP estão na forma saturada."));
+            $("#r").append($("<p>").text("H3C ─ CH2 ─ CH3    propano"));
+            $("#r").append($("<p>").text("Ambas as estruturas são formadas por ligações simples (cadeia saturada)."));
+
+            
+            $(".A").css("color", "red");
+            $(".B").css("color", "red");
+            $(".C").css("color", "red");
+            $(".D").css("color", "green");
+            $(".E").css("color", "red");
+        }
+        if($("#b").prop("checked") == true)
+        {
+            $("#resultado").text("Resposta incorreta:")
+            $("#r").append($("<p>").text("Alternativa D"));
+            $("#r").append($("<p>").text("Os hidrocarbonetos constituintes do gás GLP estão na forma saturada."));
+            $("#r").append($("<p>").text("H3C ─ CH2 ─ CH3    propano"));
+            $("#r").append($("<p>").text("Ambas as estruturas são formadas por ligações simples (cadeia saturada)."));
+            $(".A").css("color", "red");
+            $(".B").css("color", "red");
+            $(".C").css("color", "red");
+            $(".D").css("color", "green");
+            $(".E").css("color", "red");
+        }
+        if($("#c").prop("checked") == true)
+        {
+            $("#resultado").text("Resposta incorreta:")
+            $("#r").append($("<p>").text("Alternativa D"));
+            $("#r").append($("<p>").text("Os hidrocarbonetos constituintes do gás GLP estão na forma saturada."));
+            $("#r").append($("<p>").text("H3C ─ CH2 ─ CH3    propano"));
+            $("#r").append($("<p>").text("Ambas as estruturas são formadas por ligações simples (cadeia saturada)."));
+            $(".A").css("color", "red");
+            $(".B").css("color", "red");
+            $(".C").css("color", "red");
+            $(".D").css("color", "green");
+            $(".E").css("color", "red");
+        }
+        if($("#d").prop("checked") == true)
+        {
+            $("#resultado").text("Resposta correta: Parabéns!!")
+       
+            $("#r").append($("<p>").text("Os hidrocarbonetos constituintes do gás GLP estão na forma saturada."));
+            $("#r").append($("<p>").text("H3C ─ CH2 ─ CH3    propano"));
+            $("#r").append($("<p>").text("Ambas as estruturas são formadas por ligações simples (cadeia saturada)."));
+            $(".A").css("color", "red");
+            $(".B").css("color", "red");
+            $(".C").css("color", "red");
+            $(".D").css("color", "green");
+            $(".E").css("color", "red");
+        }
+        if($("#e").prop("checked") == true)
+        {
+            $("#resultado").text("Resposta incorreta:")
+            $("#r").append($("<p>").text("Alternativa D"));
+            $("#r").append($("<p>").text("Os hidrocarbonetos constituintes do gás GLP estão na forma saturada."));
+            $("#r").append($("<p>").text("H3C ─ CH2 ─ CH3    propano"));
+            $("#r").append($("<p>").text("Ambas as estruturas são formadas por ligações simples (cadeia saturada)."));
+            $(".A").css("color", "red");
+            $(".B").css("color", "red");
+            $(".C").css("color", "red");
+            $(".D").css("color", "green");
+            $(".E").css("color", "red");
+        }
+    })
+
+    $("#botao2").click(function(){
+        if($("#a1").prop("checked") == true)
+        {
+            $("#resultado2").text("Resposta correta: Parabéns!!")
+            $("#r2").append
+            (
+                
+                $("<p>").text("O gás presente nos fogões, também chamado GLP (gás liquefeito de petróleo), tem como componentes o gás metano (C4H10) e o propano (C3H8). s"),
+              
+            );
+            $(".A1").css("color", "green");
+            $(".B1").css("color", "red");
+            $(".C1").css("color", "red");
+            $(".D1").css("color", "red");
+            $(".E1").css("color", "red");
+        }
+        if($("#b1").prop("checked") == true)
+        {
+            $("#resultado2").text("Resposta incorreta: ")
+            $("#r2").append
+            (
+                $("<p>").text("A resposta correta é a letra A."),
+                $("<p>").text("O gás presente nos fogões, também chamado GLP (gás liquefeito de petróleo), tem como componentes o gás metano (C4H10) e o propano (C3H8). s"),
+              
+            );
+            $(".A1").css("color", "green");
+            $(".B1").css("color", "red");
+            $(".C1").css("color", "red");
+            $(".D1").css("color", "red");
+            $(".E1").css("color", "red");
+        }
+        if($("#c1").prop("checked") == true)
+        {
+            $("#resultado2").text("Resposta incorreta: ")
+            $("#r2").append
+            (
+                $("<p>").text("A resposta correta é a letra A."),
+                $("<p>").text("O gás presente nos fogões, também chamado GLP (gás liquefeito de petróleo), tem como componentes o gás metano (C4H10) e o propano (C3H8). s"),
+              
+            );
+            $(".A1").css("color", "green");
+            $(".B1").css("color", "red");
+            $(".C1").css("color", "red");
+            $(".D1").css("color", "red");
+            $(".E1").css("color", "red");
+        }
+        if($("#d1").prop("checked") == true)
+        {
+            $("#resultado2").text("Resposta incorreta:")
+            $("#r2").append
+            (
+                $("<p>").text("A resposta correta é a letra A."),
+                $("<p>").text("O gás presente nos fogões, também chamado GLP (gás liquefeito de petróleo), tem como componentes o gás metano (C4H10) e o propano (C3H8). s"),
+              
+            );
+            $(".A1").css("color", "green");
+            $(".B1").css("color", "red");
+            $(".C1").css("color", "red");
+            $(".D1").css("color", "red");
+            $(".E1").css("color", "red");
+        }
+        if($("#e1").prop("checked") == true)
+        {
+            $("#resultado2").text("Resposta incorreta:")
+            $("#r2").append
+            (
+                $("<p>").text("A resposta correta é a letra A."),
+                $("<p>").text("O gás presente nos fogões, também chamado GLP (gás liquefeito de petróleo), tem como componentes o gás metano (C4H10) e o propano (C3H8). s"),
+              
+            );
+            $(".A1").css("color", "green");
+            $(".B1").css("color", "red");
+            $(".C1").css("color", "red");
+            $(".D1").css("color", "red");
+            $(".E1").css("color", "red");
+        }
+    })
+
+    $("#botao3").click(function(){
+        if($("#a2").prop("checked") == true)
+        {
+            $("#resultado3").text("Resposta incorreta:")
+            $("#r3").append($("<p>").text("A resposta correta é a letra D. Um gás real aproxima-se do ideal quanto mais alta for sua temperatura e menor sua pressão."));
+            $(".A2").css("color", "red");
+            $(".B2").css("color", "red");
+            $(".C2").css("color", "red");
+            $(".D2").css("color", "green");
+            $(".E2").css("color", "red");
+        }
+        if($("#b2").prop("checked") == true)
+        {
+            $("#resultado3").text("Resposta incorreta:")
+            $("#r3").append($("<p>").text("A resposta correta é a letra D. Um gás real aproxima-se do ideal quanto mais alta for sua temperatura e menor sua pressão."));
+            $(".A2").css("color", "red");
+            $(".B2").css("color", "red");
+            $(".C2").css("color", "red");
+            $(".D2").css("color", "green");
+            $(".E2").css("color", "red");
+        }
+        if($("#c2").prop("checked") == true)
+        {
+            $("#resultado3").text("Resposta incorreta:")
+            $("#r3").append($("<p>").text("A resposta correta é a letra D. Um gás real aproxima-se do ideal quanto mais alta for sua temperatura e menor sua pressão."));
+            $(".A2").css("color", "red");
+            $(".B2").css("color", "red");
+            $(".C2").css("color", "red");
+            $(".D2").css("color", "green");
+            $(".E2").css("color", "red");
+        }
+        if($("#d2").prop("checked") == true)
+        {
+            $("#resultado3").text("Resposta correta: Parabéns!!")
+            $("#r3").append($("<p>").text(" Um gás real aproxima-se do ideal quanto mais alta for sua temperatura e menor sua pressão."));
+            $(".A2").css("color", "red");
+            $(".B2").css("color", "red");
+            $(".C2").css("color", "red");
+            $(".D2").css("color", "green");
+            $(".E2").css("color", "red");
+        }
+        if($("#e2").prop("checked") == true)
+        {
+            $("#resultado3").text("Resposta incorreta: ")
+            $("#r3").append($("<p>").text("A resposta correta é a letra D. Um gás real aproxima-se do ideal quanto mais alta for sua temperatura e menor sua pressão."));
+            $(".A2").css("color", "red");
+            $(".B2").css("color", "red");
+            $(".C2").css("color", "red");
+            $(".D2").css("color", "green");
+            $(".E2").css("color", "red");
+        }
+    })
+
+ $("#limpar").click(function(){
+        $("#a").prop("checked", false);
+        $("#b").prop("checked", false);
+        $("#c").prop("checked", false);
+        $("#d").prop("checked", false);
+        $("#e").prop("checked", false);
+        $(".A").css("color", "black");
+        $(".B").css("color", "black");
+        $(".C").css("color", "black");
+        $(".D").css("color", "black");
+        $(".E").css("color", "black");
+        $("#resultado").text("");
+        $("#r").text("");
+    });
+
+
+    $("#limpar2").click(function(){
+        $("#a1").prop("checked", false);
+        $("#b1").prop("checked", false);
+        $("#c1").prop("checked", false);
+        $("#d1").prop("checked", false);
+        $("#e1").prop("checked", false);
+        $(".A1").css("color", "black");
+        $(".B1").css("color", "black");
+        $(".C1").css("color", "black");
+        $(".D1").css("color", "black");
+        $(".E1").css("color", "black");
+        $("#resultado2").text("");
+        $("#r2").text("");
+    });
+
+    
+
+    $("#limpar3").click(function(){
+        $("#a2").prop("checked", false);
+        $("#b2").prop("checked", false);
+        $("#c2").prop("checked", false);
+        $("#d2").prop("checked", false);
+        $("#e2").prop("checked", false);
+        $(".A2").css("color", "black");
+        $(".B2").css("color", "black");
+        $(".C2").css("color", "black");
+        $(".D2").css("color", "black");
+        $(".E2").css("color", "black");
+        $("#resultado3").text("");
+        $("#r3").text("");
+    });
+
+});
